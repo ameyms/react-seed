f8c0d62d26e17f9a5ffd210359a404a56ecb488d
Amey Sakhadeo <me@ameyms.com>
2015-04-15 22:10:13 -0700

    Code formatting

--- build/webpack/dev-server.js
+++ build/webpack/dev-server.js
@@ -1,20 +1,26 @@
-module.exports = {start: function() {var webpack = require('webpack'),
+module.exports = {
+
+    start: function() {
+        var webpack = require('webpack'),
             WebpackDevServer = require('webpack-dev-server'),
             chalk = require('chalk'),
             config = require('./dev.config');
 
-        new WebpackDevServer(webpack(config), {publicPath: config.output.publicPath,
+        new WebpackDevServer(webpack(config), {
+            publicPath: config.output.publicPath,
             hot: true,
             historyApiFallback: true,
             quiet: false,
             noInfo: false,
             watchDelay: 300,
             contentBase: 'dist/bundle',
-            headers: {'X-SNAS-Auth': 'yes'},
+            headers: {'X-React-Auth': 'yes'},
             stats: {colors: true}
-        } ).listen(config.devserver.port, 'localhost', function(err) {if (err) {console.log(chalk.bgRed.bold(err) );
+        }).
+        listen(config.devserver.port, 'localhost', function(err) {
+            if (err) {
+                console.log(chalk.bgRed.bold(err) );
             }
-
             console.log(chalk.white('Listening at',
                 chalk.black.bgWhite('localhost:' + config.devserver.port) ) );
         } );
