65a167e651af5e6b110fe57a67d2c8b9b1d9ead2
Amey Sakhadeo <me@ameyms.com>
2015-05-11 22:51:16 -0700

    Add an eslint disable

--- build/jest/Sample-test.jsx
+++ build/jest/Sample-test.jsx
@@ -4,8 +4,10 @@ jest.dontMock('../Dummy');
 describe('Dummy', function() {
 
     beforeEach(function() {
+        /*eslint-disable no-undef*/
         React = require('react/addons');
         TestUtils = React.addons.TestUtils;
+        /*eslint-enable no-undef*/
     });
 
     it('dummy test', function() {
