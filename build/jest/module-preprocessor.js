f3c173cc2c9622526effd228aec41e72f0ae727c
Amey Sakhadeo <me@ameyms.com>
2015-07-12 09:45:49 -0700

    Use babel to transform code in jest`s module preprocessor

--- build/jest/module-preprocessor.js
+++ build/jest/module-preprocessor.js
@@ -1,5 +1,5 @@
-var ReactTools = require('react-tools'),
-    jsPath = /.*\/react\-seed\/src\/js\/[a-z]+(?:\/__tests__)?\/[a-zA-Z]+(?:\-test)?\.jsx$/,
+var babel = require('babel'),
+    jsPath = /.*\/react\-seed\/src\/js\/[a-z]+(?:\/__tests__)?\/[a-zA-Z]+(?:\-test)?\.jsx?$/,
     lessPath = /.*\/react\-seed\/src\/less\/[a-z]+\/[a-zA-Z]+\.less$/;
 
 module.exports = {
@@ -11,7 +11,16 @@ module.exports = {
         if (jsPath.test(filename)) {
 
             src = src.replace(/^.*require\(\s*'styles\/\w+\/\w+\.less'\s*\);/g, '');
-            return ReactTools.transform(src);
+            src = src.replace(/^\s*import\s+'styles\/\w+\/\w+\.less'\s*;/g, '');
+
+            src = babel.transform(src, {
+                modules: 'common',
+                stage: 2
+            }).code;
+
+            // src = ReactTools.transform(src, {});
+
+            return src;
         } else if (lessPath.test(filename)) {
             return dummyLessModule;
         } else {
