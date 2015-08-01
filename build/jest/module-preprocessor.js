406081bc11c6d9145130de5cd26a294b104a51c4
Amey Sakhadeo <me@ameyms.com>
2015-08-01 14:51:06 -0700

    Use babel-jest as a jest module preprocessor

--- build/jest/module-preprocessor.js
+++ build/jest/module-preprocessor.js
@@ -1,6 +1,7 @@
-var babel = require('babel'),
-    jsPath = /.*\/react\-seed\/src\/js\/[a-z]+(?:\/__tests__)?\/[a-zA-Z]+(?:\-test)?\.jsx?$/,
+var babel = require('babel-jest'),
+    jsPath = /.*\/react\-seed\/src\/js(?:\/[a-z]+)?(?:\/__tests__)?\/[a-zA-Z]+(?:\-test)?\.jsx?$/,
     lessPath = /.*\/react\-seed\/src\/less\/[a-z]+\/[a-zA-Z]+\.less$/;
+
 
 module.exports = {
 
@@ -13,14 +14,7 @@ module.exports = {
             src = src.replace(/^.*require\(\s*'styles\/\w+\/\w+\.less'\s*\);/g, '');
             src = src.replace(/^\s*import\s+'styles\/\w+\/\w+\.less'\s*;/g, '');
 
-            src = babel.transform(src, {
-                modules: 'common',
-                stage: 2
-            }).code;
-
-            // src = ReactTools.transform(src, {});
-
-            return src;
+            return babel.process(src, filename);
         } else if (lessPath.test(filename)) {
             return dummyLessModule;
         } else {
